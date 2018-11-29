e2b46ea25ffca15dde83d5e5d3a81e3e301a0c69
Bhavik Thakkar <bhavik.t@lucideustech.com>
2018-11-29 22:32:09 +0530

    Add geolocation via geolocation API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,4 +11,17 @@ describe('generateMessage', () => {
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text});
     })
+});
+
+describe('generateLocationMessage', () => {
+    it('should create correct location object', () => {
+        var from = 'Bhavik';
+        var latitude = '28.560302099999998';
+        var longitude = '77.2534251';
+        var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+        var message = generateLocationMessage(from, latitude, longitude);
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
+    });
 });
