efd870c2067cef10922bbf8954e23b102bfceeb9
Bhavik Thakkar <bhavik.t@lucideustech.com>
2018-12-10 22:42:31 +0530

    Add Users class and updateUserList event

---
+++ server/utils/users.js
@@ -0,0 +1,58 @@
+var _ = require('lodash');
+
+[{
+    id: '/ahsdfhasefbj',
+    name: 'Bhavik',
+    room: 'The Office fans'
+}]
+
+class Users {
+    constructor () {
+        this.users = [];
+    }
+
+    addUser (id, name, room) {
+        var user = {id, name, room};
+        this.users.push(user);
+        return user;
+    }
+
+    removeUser (id) {
+        var user = this.getUser(id);
+        this.users =  this.users.filter((user) => user.id !== id);
+
+        return user;
+    }
+
+    getUser (id) {
+        var user = _.find(this.users, {id});
+
+        return user
+    }
+
+    getUserList (room) {
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+
+        return namesArray;
+    }
+}
+
+module.exports = {Users};
+
+// class Person {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     getUserDescription () {
+//         return `${this.name} is ${this.age} year(s) old`
+//     }
+// }
+
+// var me = new Person('Bhavik', '25');
+
+// console.log(`${me.name} ${me.age}`);
+// var description = me.getUserDescription();
+// console.log(description);
